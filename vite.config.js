19dd1b19055b0dcbf135cdc8fe28cddae45426b4
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2023-09-19 01:55:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mithun-750.github.io/"
+  base: "https://Mithun-750.github.io/"
 })
